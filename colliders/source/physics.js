a327cc55a82552dd5b489e56e9805b6f496867da
Percy <perkyrocca@yahoo.com>
2020-02-09 17:32:32 -0500

    Add files via upload

--- colliders/source/physics.js
+++ colliders/source/physics.js
@@ -55,15 +55,22 @@
 	//then applies friction
 	Sphere.tempHandleSphere = function(s1, s2){
 		var inside = s1.violation(s2);//how far they are inside eachother
-		var v = {x:s2.pos.x - s1.pos.x, y:s2.pos.y - s1.pos.y};//vector between them
+		
+		if(inside > 0) return; 
+		
+		
+		var v = new Vec(s2.pos.x - s1.pos.x, s2.pos.y - s1.pos.y).normalize();//vector between them
 		var vm = Math.sqrt(v.x * v.x + v.y *v.y);
 		if (vm == 0 ) throw "0 division";
-		v.x /= vm, v.y /= vm;
 		s1.pos.x += v.x * inside / 2, s1.pos.y += v.y * inside / 2;
 		s2.pos.x -= v.x * inside / 2, s2.pos.y -= v.y * inside / 2;
 		
 		//at this point they should no longer be contacting
+		//TODO mirror their velocities
+		var pv = new Vec(v.y,-v.x);//perpendicular to line between them, normalized
 		
+		s1.speed = new Vec(s1.speed.x, s1.speed.y).mirror(v);
+		s2.speed = new Vec(s2.speed.x, s2.speed.y).mirror(v);
 	};
 	
 	
@@ -79,7 +86,7 @@
 			return d - (this.radius + sphere.radius);
 		},
 		
-		isColliding:function(sphere){
+		isColliding:function(sphere){//somethings wrong
 			var dx = this.pos.x - sphere.pos.x, dy = this.pos.y - sphere.pos.y, d = dx * dx + dy * dy;
 			return d < this.radius * this.radius  + sphere.radius * sphere.radius;
 		}
---
+++ colliders/source/vec2.js
@@ -0,0 +1,32 @@
+(function(){
+	
+	
+function inherit(){
+	
+};
+
+
+function Vec2(x, y){
+	this.x = x ||0, this.y = y ||0;
+}
+
+
+//TODO inlining this stuff might make it more efficient
+
+Vec2.prototype = {
+	add:function(v){return new Vec2(this.x + v.x, this.y + v.y)},
+	subtract:function(v){return this.add(new Vec2(-v.x, -v.y))},
+	dot(v){return this.x * v.x + this.y * v.y},
+	magnitude(squared){return (squared)? this.x * this.x + this.y * this.y : Math.sqrt(this.magnitude(true))},
+	normalize(){var m = this.magnitude(); return new Vec2(this.x/m, this.y/m)},
+	mirror(n){
+		n = n.normalize(), multiplier = 2 * this.dot(n);
+		return this.subtract(new Vec2(n.x * multiplier, n.y * multiplier));
+	}//mirrors this vector on argument vector
+	
+	
+};
+
+window.Vec = Vec2;
+
+})()
